06400debfdd732752e1a7e561f0d4ebf7bfb2962
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-05-26 18:42:07 +0200

    Added a test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -9,5 +9,17 @@ test("login with correct credentials", async ({ page }) => {
   await page.getByTestId("login-button").click();
   await page.getByTestId("user-name").click();
 
-  await expect(page.getByTestId("user-name")).toHaveText("Jan DemobankowyX");
+  await expect(page.getByTestId("user-name")).toHaveText("Jan Demobankowy");
+});
+
+test("login with incorrect username", async ({ page }) => {
+  await page.goto("https://demo-bank.vercel.app/");
+  await page.getByTestId("login-input").click();
+  await page.getByTestId("login-input").fill("asdf");
+  await page.getByTestId("password-input").click();
+  await page.getByTestId("error-login-id").click();
+
+  await expect(page.getByTestId("error-login-id")).toHaveText(
+    "identyfikator ma min. 8 znaków"
+  );
 });
