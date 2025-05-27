ca60cb835bb2f5b580b45f8aeb64cd07404f0cce
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-05-28 00:28:50 +0200

    Moved the whole page login to beforeEach() in pulpit.spec.js

--- tests/pulpit.spec.js
+++ tests/pulpit.spec.js
@@ -3,14 +3,17 @@ import { test, describe, expect } from "@playwright/test";
 test.describe("Pulpit", () => {
   test.beforeEach(async ({ page }) => {
     const url = "https://demo-bank.vercel.app/";
+    const username = "asdfasdf";
+    const password = "qweqweqw";
+
     await page.goto(url);
+    await page.getByTestId("login-input").fill(username);
+    await page.getByTestId("password-input").fill(password);
+    await page.getByTestId("login-button").click();
   });
 
   test("Quick payment with correct data", async ({ page }) => {
     // Arrange
-    const username = "asdfasdf";
-    const password = "qweqweqw";
-
     const option = "2";
     const amount = "123";
     const transferTitle = "pizza";
@@ -18,10 +21,6 @@ test.describe("Pulpit", () => {
     const expectedMessage = `Przelew wykonany! ${expectedReceiverName} - ${amount},00PLN - ${transferTitle}`;
 
     // Act
-    await page.getByTestId("login-input").fill(username);
-    await page.getByTestId("password-input").fill(password);
-    await page.getByTestId("login-button").click();
-
     await page.locator("#widget_1_transfer_receiver").selectOption(option);
     await page.locator("#widget_1_transfer_amount").fill(amount);
     await page.locator("#widget_1_transfer_title").fill(transferTitle);
@@ -34,18 +33,11 @@ test.describe("Pulpit", () => {
 
   test("Phone topup with correct data", async ({ page }) => {
     // Arrange
-    const username = "asdfasdf";
-    const password = "qweqweqw";
-
     const option = "502 xxx xxx";
     const amount = "123";
     const expectedMessage = `Doładowanie wykonane! ${amount},00PLN na numer ${option}`;
 
     // Act
-    await page.getByTestId("login-input").fill(username);
-    await page.getByTestId("password-input").fill(password);
-    await page.getByTestId("login-button").click();
-
     await page.locator("#widget_1_topup_receiver").selectOption(option);
     await page.locator("#widget_1_topup_amount").fill(amount);
     await page.locator("#widget_1_topup_agreement").click();
