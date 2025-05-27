a0491b1a942a3c8762df34fa6a807d47c23df1c8
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-05-27 15:13:14 +0200

    Added a test

--- tests/pulpit.spec.js
+++ tests/pulpit.spec.js
@@ -17,4 +17,21 @@ test.describe("Pulpit", () => {
       "Przelew wykonany! Chuck Demobankowy - 123,00PLN - pizza"
     );
   });
+
+  test.only("Phone topup with correct data", async ({ page }) => {
+    await page.goto("https://demo-bank.vercel.app/");
+    await page.getByTestId("login-input").fill("asdfasdf");
+    await page.getByTestId("password-input").fill("password");
+    await page.getByTestId("login-button").click();
+
+    await page.locator("#widget_1_topup_receiver").selectOption("502 xxx xxx");
+    await page.locator("#widget_1_topup_amount").fill("123");
+    await page.locator("#widget_1_topup_agreement").click();
+    await page.locator("#execute_phone_btn").click();
+    await page.getByTestId("close-button").click();
+
+    await expect(page.locator("#show_messages")).toHaveText(
+      "Doładowanie wykonane! 123,00PLN na numer 502 xxx xxx"
+    );
+  });
 });
