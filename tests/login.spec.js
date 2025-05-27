13e1a33e48143ba688df38aa690c6a245d969b48
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-05-27 13:55:57 +0200

    Removed redundant test steps

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,22 +3,17 @@ import { test, expect } from "@playwright/test";
 test.describe("User login to Demobank", () => {
   test("Successful login with correct credentials", async ({ page }) => {
     await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").click();
     await page.getByTestId("login-input").fill("asdfasdf");
-    await page.getByTestId("password-input").click();
     await page.getByTestId("password-input").fill("qweqweqw");
     await page.getByTestId("login-button").click();
-    await page.getByTestId("user-name").click();
 
     await expect(page.getByTestId("user-name")).toHaveText("Jan Demobankowy");
   });
 
   test("Unsuccessful login with too short username", async ({ page }) => {
     await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").click();
     await page.getByTestId("login-input").fill("asdf");
-    await page.getByTestId("password-input").click();
-    await page.getByTestId("error-login-id").click();
+    await page.getByTestId("login-input").blur();
 
     await expect(page.getByTestId("error-login-id")).toHaveText(
       "identyfikator ma min. 8 znaków"
@@ -27,12 +22,9 @@ test.describe("User login to Demobank", () => {
 
   test("Unsuccessful login with too short password", async ({ page }) => {
     await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").click();
     await page.getByTestId("login-input").fill("asdfasdf");
-    await page.getByTestId("password-input").click();
     await page.getByTestId("password-input").fill("qweqweq");
     await page.getByTestId("password-input").blur();
-    await page.getByTestId("error-login-password").click();
 
     await expect(page.getByTestId("error-login-password")).toHaveText(
       "hasło ma min. 8 znaków"
